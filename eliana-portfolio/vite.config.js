b85ad1778c66e6d1d065eeb951eeb1d4bef6bdc8
ElianaScipt <eliana.liantonio@gmail.com>
2025-01-21 07:14:25 -0500

    debugged

--- eliana-portfolio/vite.config.js
+++ eliana-portfolio/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/eliana-portfolio/',
   build: {
     rollupOptions: {
       input: './index.html',
