1dfc4b835e451dd2cb764149693e34f38cdba37b
ElianaScript <eliana.liantonio@gmail.com>
2025-01-20 22:11:29 -0500

    Update vite.config.js

--- eliana-portfolio/vite.config.js
+++ eliana-portfolio/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/eliana-portfolio/', 
   build: {
     rollupOptions: {
-      input: 'index.html',
+      input: './index.html',
     },
   },
   plugins: [react()],
