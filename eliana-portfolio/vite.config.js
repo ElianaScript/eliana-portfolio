4f4f5e7a125b5f1cb310d0a917934cac18a98262
ElianaScipt <eliana.liantonio@gmail.com>
2025-01-21 06:24:29 -0500

    save local changes before pulling

--- eliana-portfolio/vite.config.js
+++ eliana-portfolio/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: 'index.html',
+      input: './index.html',
     },
   },
   plugins: [react()],
-});
+})
