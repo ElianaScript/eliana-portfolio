1b6005a79e22e70b40bc4d4a4804d39a5bfa9909
ElianaScipt <eliana.liantonio@gmail.com>
2025-01-21 14:22:04 -0500

    new index.html

--- eliana-portfolio/vite.config.js
+++ eliana-portfolio/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/eliana-portfolio/eliana-portfolio',
+  base: '/',
   build: {
     rollupOptions: {
       input: './index.html',
